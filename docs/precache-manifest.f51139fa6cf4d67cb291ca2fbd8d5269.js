dff087f86c2ef02a1a2b40c8680bcf4406cac03a
fuxingZhang <502545703@qq.com>
2018-10-19 18:28:34 +0800

    update

---
+++ docs/precache-manifest.f51139fa6cf4d67cb291ca2fbd8d5269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d5e4e96c62789f1fa3f",
+    "url": "./demo/static/js/runtime~main.8d5e4e96.js"
+  },
+  {
+    "revision": "ef3ff1e686988e60d9c7",
+    "url": "./demo/static/js/main.ef3ff1e6.chunk.js"
+  },
+  {
+    "revision": "65a2a3e43f3316624a31",
+    "url": "./demo/static/js/1.65a2a3e4.chunk.js"
+  },
+  {
+    "revision": "ef3ff1e686988e60d9c7",
+    "url": "./demo/static/css/main.9a05e8b4.chunk.css"
+  },
+  {
+    "revision": "15d029e7de5b50f1348d2f8a0b966d16",
+    "url": "./demo/index.html"
+  }
+];
